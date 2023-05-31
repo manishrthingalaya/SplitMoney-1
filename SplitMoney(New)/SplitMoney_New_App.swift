64d715b96c6200b57c82ea7a62925c67eb2200c5
Manish T <manish.t@robosoftin.com>
2023-05-31 11:28:07 +0530

    Initial Commit

---
+++ SplitMoney(New)/SplitMoney_New_App.swift
@@ -0,0 +1,17 @@
+//
+//  SplitMoney_New_App.swift
+//  SplitMoney(New)
+//
+//  Created by Manish T on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SplitMoney_New_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
